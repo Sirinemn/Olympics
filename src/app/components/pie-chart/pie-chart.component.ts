b1c47244c3757f442a2e6c4a40e18c914e68d253
Sirinemn <sirinemn@outlook.fr>
2025-01-20 10:11:52 +0100

    implements pie-chat .ts

--- src/app/components/pie-chart/pie-chart.component.ts
+++ src/app/components/pie-chart/pie-chart.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { ChartService } from '../../core/services/chart.service';
+import { OlympicService } from '../../core/services/olympic.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-pie-chart',
@@ -7,6 +10,47 @@ import { Component } from '@angular/core';
   templateUrl: './pie-chart.component.html',
   styleUrl: './pie-chart.component.scss'
 })
-export class PieChartComponent {
+export class PieChartComponent implements OnInit, OnDestroy{
+  
+  medals: number[] = [];
+  listCountry: string[] = [];
+  numMedal: number[] = [];
+  public numberOfCountries: number = 0;
+  public numberOfJo: number = 0;
+  private httpSubscription!: Subscription;
+  public errorMessage: string ="";
 
+  constructor(private chartService: ChartService, private olymoicService: OlympicService) {}
+ 
+  ngOnInit(): void {
+    this.chartData();
+  }
+
+  public chartData(): void {
+    this.httpSubscription = this.olymoicService.loadInitialData().subscribe((result) => {
+      let medalByCountry = new Map();
+
+      for (let i = 0; i < result.length; i++) {
+        this.numberOfJo += result[i].participations.length;
+        let listMedal = result[i].participations.map((m) => m.medalsCount);
+        let sumMedal = listMedal.reduce(
+          (accumulator, currentValue) => accumulator + currentValue, 0
+        );
+        medalByCountry.set(result[i].country, sumMedal);
+      }
+
+      medalByCountry.forEach((value: number, key: string) => {
+        this.numMedal.push(value);
+        this.listCountry.push(key);
+      });
+
+      this.numberOfCountries = this.listCountry.length;
+      this.chartService.createPieChart(this.listCountry, this.numMedal);
+    }, (error) =>{
+      this.errorMessage = error;
+    });
+  } 
+  ngOnDestroy(): void {
+    this.httpSubscription.unsubscribe();
+  }
 }
