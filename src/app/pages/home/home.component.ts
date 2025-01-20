7170a889a5d4ad32ce05ecc93ec6359d62ef2204
Sirinemn <sirinemn@outlook.fr>
2025-01-20 09:15:41 +0100

    add routes

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { MainComponent } from "../../components/main/main.component";
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [MainComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
