a7bb35ea2dc53bcab9466f7858bb4d05c1dc182c
Sirinemn <sirinemn@outlook.fr>
2025-01-20 10:53:34 +0100

    implements line chart component

--- src/app/components/line-chart/line-chart.component.ts
+++ src/app/components/line-chart/line-chart.component.ts
@@ -1,4 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { OlympicCountry } from '../../core/interface/olympic-country.interface';
+import { Subscription } from 'rxjs';
+import { ChartService } from '../../core/services/chart.service';
+import { OlympicService } from '../../core/services/olympic.service';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-line-chart',
@@ -7,6 +12,65 @@ import { Component } from '@angular/core';
   templateUrl: './line-chart.component.html',
   styleUrl: './line-chart.component.scss'
 })
-export class LineChartComponent {
+export class LineChartComponent implements OnInit, OnDestroy{
+  medals_number: number[] = [];
+  years: number[] = [];
+  public selectedCountry = '';
+  finalCountry!: OlympicCountry;
+  public numberOfEntries: Number = 0;
+  public totalNumberOfAthletes: Number = 0;
+  public totalNumberOfMedals: Number = 0;
+  private httpSubscription!: Subscription;
 
+
+  constructor(
+    private chartService: ChartService,
+    private olympicService: OlympicService,
+    private activatedRoute: ActivatedRoute,
+    private location: Location
+  ) {}
+  
+  ngOnInit(): void {
+    this.activatedRoute.params.subscribe((paramsId) => {
+      this.selectedCountry = paramsId['country'];
+    });
+
+    this.chartData();
+  }
+  public back() {
+    window.history.back();
+  }
+  chartData() {
+    this.httpSubscription = this.olympicService.loadInitialData().subscribe((olympics) => {
+        olympics
+          .filter((olympic) => olympic.country === this.selectedCountry)
+          .map((country) => (this.finalCountry = country));
+        let numbreMedalByYear = new Map();
+        this.finalCountry.participations.map((participation) => {
+          numbreMedalByYear.set(participation.year, participation.medalsCount);
+        });
+        numbreMedalByYear.forEach((value: number, key: number) => {
+          this.years.push(key);
+          this.medals_number.push(value);
+        });
+        this.chartService.createLineChart(this.years, this.medals_number);
+        this.totalNumberOfMedals = this.medals_number.reduce(
+          (acc, curr) => acc + curr,
+          0
+        );
+        let listNumberOfAthletes = this.finalCountry.participations.map(
+          (value) => value.athleteCount
+        );
+        this.totalNumberOfAthletes = listNumberOfAthletes.reduce(
+          (acc, curr) => acc + curr,
+          0
+        );
+        this.numberOfEntries = this.finalCountry.participations.length;
+      }, (error) =>{
+        console.log(error);
+      });
+  }
+  ngOnDestroy(): void {
+    this.httpSubscription.unsubscribe();
+  }
 }
